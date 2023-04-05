6b62a545a4444bd50f69f80c574e75639ed43107
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-05 12:09:02 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,16 +1,16 @@
 echo "
 [app]
 address = \"0.0.0.0:${PORT}\"
-admin_username = \"${ADMIN_USERNAME}\"
-admin_password = \"${ADMIN_PASSWORD}\"
+admin_username = \"${Peter}\"
+admin_password = \"${yang545}\"
 # Database.
 [db]
-host = \"${PGHOST}\"
-port = ${PGPORT}
-user = \"${PGUSER}\"
-password = \"${PGPASSWORD}\"
+host = \"${containers-us-west-91.railway.app}\"
+port = ${6823}
+user = \"${postgres}\"
+password = \"${9ZN1xjjYjoZhw2mFJd69}\"
 database = \"${PGDATABASE}\"
-ssl_mode = \"disable\"
+ssl_mode = \"postgresql://postgres:9ZN1xjjYjoZhw2mFJd69@containers-us-west-91.railway.app:6823/railway\"
 max_open = 25
 max_idle = 25
 max_lifetime = \"300s\"
